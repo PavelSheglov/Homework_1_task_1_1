f119c8f673c209565f583e2ba5b14345132d9082
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-16 22:29:01 +0700

    Задача по сложению 2-х чисел

--- lec_1_hw_1_1/Program.cs
+++ lec_1_hw_1_1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lec_1_hw_1_1
 {
@@ -10,6 +6,18 @@ namespace lec_1_hw_1_1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Введите число 1:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите число 2:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Сумма числа {0} и {1} равна {2}", num1, num2, num1 + num2);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Ошибка при вводе чисел!!!");
+            }
         }
     }
 }
